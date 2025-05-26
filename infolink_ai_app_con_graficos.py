9c56b8c74ce3d436f5ba9a053dfcb000650467c1
COM2-GRUPO2 <shaticabonita03@gmail.com>
2025-05-25 23:02:59 -0500

    Update infolink_ai_app_con_graficos.py

--- infolink_ai_app_con_graficos.py
+++ infolink_ai_app_con_graficos.py
@@ -37,10 +37,10 @@ elif pin_value < 0.25:
 else:
     st.success("✅ Buen nivel de información en el mercado.")
     
-st.markdown("Relación entre α y PIN ")
+st.markdown("- Relación entre α y PIN ")
 
 img_formula = Image.open("grafico_formula_pin.png")
-st.image(img_formula, caption="Relación entre α y PIN", use_container_width=True)
+st.image(img_formula, caption="Relación entre α y PIN", width=900)
 
 st.markdown("- α: tasa de traders con información (usuarios bien comunicados)")
 st.markdown("- ε: tasa de traders sin información (ruido)")
